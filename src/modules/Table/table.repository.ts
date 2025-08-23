e40470521a2e9a1f69c599834e8c80fbe37953d6
PermAdut <hdvhdvhdvwww@gmail.com>
2025-08-23 11:59:04 +0300

    feat: implemented table repository with main crud functions

---
+++ src/modules/Table/table.repository.ts
@@ -0,0 +1,38 @@
+import { QueryResult } from 'pg'
+import { ITable } from './types/table.interface'
+import { pool } from '../../utils/database.connection'
+import { AppError } from '../../middlewares/error.middleware'
+import { HttpStatusCode } from '../../utils/statusCodes'
+import { ErrorMessages } from '../../utils/errorMessage'
+
+class TableRepository {
+  async getTablesByUserId(id: number): Promise<ITable[]> {
+    const queryResult: QueryResult<ITable> = await pool.query('SELECT * FROM "Tables" WHERE "userId" = $1', [id])
+    return queryResult.rows
+  }
+
+  async deleteTableById(id: number): Promise<ITable> {
+    const queryResult: QueryResult<ITable> = await pool.query(
+      `DELETE FROM "Tables" WHERE id = $1 RETURNING id, "userId", name`,
+      [id],
+    )
+    if (!queryResult.rows.length) {
+      throw new AppError(HttpStatusCode.NOT_FOUND, ErrorMessages.TABLE_NOT_FOUND)
+    }
+    return queryResult.rows[0]
+  }
+
+  async updateTable(id: number, name: string): Promise<ITable> {
+    const queryResult: QueryResult<ITable> = await pool.query(
+      `UPDATE "Tables" SET name = $2 WHERE id = $1 RETURNING id, "userId", name`,
+      [id, name],
+    )
+    if (!queryResult.rows.length) {
+      throw new AppError(HttpStatusCode.NOT_FOUND, ErrorMessages.TABLE_NOT_FOUND)
+    }
+    return queryResult.rows[0]
+  }
+}
+
+const tableRepositoryInstance = new TableRepository()
+export default tableRepositoryInstance
--- src/utils/errorMessage.ts
+++ src/utils/errorMessage.ts
@@ -1,12 +1,20 @@
 export enum ErrorMessages {
+  //default
   INTERNAL_SERVER_ERROR = 'Iternal Server Error',
+
+  //auth token logic
   INVALID_OR_EXPIRED_ACCESS_TOKEN = 'Invalid or expired access token',
   INVALID_OR_EXPIRED_REFRESH_TOKEN = 'Invalid or expired refresh token',
   MISSING_OR_INVALID_AUTH_HEADER = 'Authorization header missing or invalid',
+
+  //auth logic
   PASSWORDS_MUST_BE_THE_SAME = 'Password and repeated password must be the same',
   INVALID_USERNAME = 'Provided username is invalid',
   INVALID_PASSWORD = 'Provided password is invalid',
   REFRESH_TOKEN_NOT_PROVIDED = 'Refresh token did not provided',
   USER_NOT_FOUND = 'User does not exist',
   USERNAME_OR_EMAIL_NOT_ALLOWED = 'User with this username or email exists',
+
+  //table
+  TABLE_NOT_FOUND = 'Table with this id does not found',
 }
